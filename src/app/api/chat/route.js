4b749b71fe31b4067542d49e5100140e9f651bfe
Veronica Rojas <ar_veronica@yahoo.com>
2023-08-29 12:44:38 +0100

    data object sending as empty

--- src/app/api/chat/route.js
+++ src/app/api/chat/route.js
@@ -1,26 +1,32 @@
 import { NextResponse } from 'next/server';
 import axios from 'axios';
 
-export async function POST() {
+export async function POST(req) {
   try {
+    const { body } = req;
 
-    const response = await axios.post(
-      'https://api.openai.com/v1/chat/completions',
-      {
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${process.env.NEXT_PUBLIC_OPENAI_API_KEY}`,
-        },
-      }
-    );
+    let config = {
+      method: 'post',
+      maxBodyLength: Infinity,
+      url: 'https://api.openai.com/v1/chat/completions',
+      headers: { 
+        'Content-Type': 'application/json', 
+        'Authorization': `Bearer ${process.env.NEXT_PUBLIC_OPENAI_API_KEY}`
+      },
+      data : body
+    };
+
+    const response = await axios.request(config);
+
+  
 
     const data = response.data;
-    return NextResponse.json(data), {
+    return new NextResponse.json(data), {
       status: 200,
     };
   } catch (error) {
     console.log(error);
-    return NextResponse('An error occurred while fetching data.', {
+    return new NextResponse('An error occurred while fetching data.', {
       status: 500,
     });
   }
